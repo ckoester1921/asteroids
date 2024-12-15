129a79c319f3b819a31318a31372569dcaed4c90
ckoester1921 <ckoester1921@gmail.com>
2024-12-15 13:52:25 -0700

    visualize player on the screen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import *
+
 
 def main():
     pygame.init()
@@ -8,12 +10,24 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2), PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill((1,1,1))
+        player.draw(screen)
         pygame.display.flip()
+
+        player.draw(screen)
+
+        dt = game_clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
