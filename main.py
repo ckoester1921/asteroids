fd115ed37d43e124b7d2cfb5648cde3e054a9309
ckoester1921 <ckoester1921@gmail.com>
2024-12-15 14:18:15 -0700

    added movement and rotation

--- main.py
+++ main.py
@@ -20,13 +20,18 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        player.update(dt)
+
         screen.fill((1,1,1))
         player.draw(screen)
+        
         pygame.display.flip()
 
         player.draw(screen)
 
         dt = game_clock.tick(60) / 1000
+
+        
         
 
 if __name__ == "__main__":
